087124aaaa438f61266694f744df70a7c00d4b62
Jason Watkins <goushoryuken@gmail.com>
2022-07-12 14:01:46 -0700

    basic check

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// var testSquare = document.getElementById("square1A");
+const board = [null, null, null, null, null, null, null, null, null];
+
+setX = function(space) {
+  board[space] = "x";
+}
+
+setO = function(space) {
+  board[space] = "o";
+}
+
+checkBoard = function () {
+
+  if (!board.includes(null)) {
+    document.getElementById("right").innerHTML = 
+      "<h1>DRAW</h1";
+  }
+}
+
 toggleX = function (evt) {
   if (evt.currentTarget.children.length == 0) {
     x = document.createElement("img");
@@ -6,6 +23,10 @@ toggleX = function (evt) {
     x.src = 'assets/x.svg';
     evt.currentTarget.appendChild(x);
     x.classList.add("show");
+    setX(evt.currentTarget.id.split("-"));
+    oTurn();
+    console.log(board);
+    checkBoard();
   }
 }
 
@@ -16,6 +37,10 @@ toggleO = function (evt) {
     x.src = 'assets/o.svg';
     evt.currentTarget.appendChild(x);
     x.classList.add("show");
+    setO(evt.currentTarget.id.split("-"));
+    xTurn();
+    console.log(board);
+    checkBoard();
   }
 }
 
@@ -29,9 +54,16 @@ xTurn = function () {
 
 oTurn = function () {
   document.querySelectorAll(".square").forEach(square => {
-    square.removeEventListener('click', toggleX );
-    square.removeEventListener('click', toggleO );
-    square.addEventListener('click', toggleO );
+    square.removeEventListener('click', toggleX);
+    square.removeEventListener('click', toggleO);
+    square.addEventListener('click', toggleO);
+  })
+}
+
+noTurn = function () {
+  document.querySelectorAll(".square").forEach(square => {
+    square.removeEventListener('click', toggleX);
+    square.removeEventListener('click', toggleO);
   })
 }
 
